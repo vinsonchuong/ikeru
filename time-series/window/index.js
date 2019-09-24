c7f39e6afd78a0c61f495af84a34585a6315bf37
Vinson Chuong <vinsonchuong@gmail.com>
2019-09-24 14:44:28 -0700

    feat(window): Enable looking at a particular time range of a time series

---
+++ time-series/window/index.js
@@ -0,0 +1,23 @@
+/* @flow */
+import type { TimeSeries } from '../'
+
+export default function<Data>(
+  timeSeries: TimeSeries<Data>,
+  start: Date,
+  end: Date
+): TimeSeries<Data> {
+  let startIndex = 0
+  while (
+    timeSeries[startIndex].time < start &&
+    startIndex < timeSeries.length
+  ) {
+    startIndex++
+  }
+
+  let endIndex = timeSeries.length - 1
+  while (timeSeries[endIndex].time > end && endIndex > 0) {
+    endIndex--
+  }
+
+  return timeSeries.slice(startIndex, endIndex + 1)
+}
---
+++ time-series/window/index.test.js
@@ -0,0 +1,21 @@
+/* @flow */
+import test from 'ava'
+import { window } from 'ikeru/time-series'
+
+test('looking at a particular time range of a time series', t => {
+  const series = [
+    { time: new Date('2018-12-30') },
+    { time: new Date('2018-12-31') },
+    { time: new Date('2019-01-01') },
+    { time: new Date('2019-01-03') },
+    { time: new Date('2019-01-31') },
+    { time: new Date('2019-02-01') },
+    { time: new Date('2019-02-03') }
+  ]
+
+  t.deepEqual(window(series, new Date('2019-01-01'), new Date('2019-01-31')), [
+    { time: new Date('2019-01-01') },
+    { time: new Date('2019-01-03') },
+    { time: new Date('2019-01-31') }
+  ])
+})
